2c60eea6f5a94562d4cdb8825231bdb3c28e5a89
Javitemek <javierbarraganh@gmail.com>
2018-05-15 11:46:16 +0200

    Añadida libreria y ficheros nuevos

---
+++ Practica_1/src/cc/qp/QuePasaMonitor.java
@@ -0,0 +1,36 @@
+package cc.qp;
+//Grupo: Javier Barragán Haro (y160253), Raul Carbajosa Gonzalez (Pon tu puta matricula cabron)
+import es.upm.babel.cclib.*;
+public class QuePasaMonitor implements QuePasa{
+
+	@Override
+	public void crearGrupo(int creadorUid, String grupo) throws PreconditionFailedException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void anadirMiembro(int creadorUid, String grupo, int nuevoMiembroUid) throws PreconditionFailedException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void salirGrupo(int miembroUid, String grupo) throws PreconditionFailedException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mandarMensaje(int remitenteUid, String grupo, Object contenidos) throws PreconditionFailedException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Mensaje leer(int uid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
